bed98eabb62bf2783dc69521c5bab29887fe7173
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2026-09-04 09:59:02 +0800

    prac: 1

--- hi.py
+++ hi.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+x, y, z = 1, 2, 3
+
+print(x)
+print(y)
+print(z)
+
+name = input("What is your stupid name, dork? ")
+
+print("Hello, ", name, ". Nya")
+
+a, b = 10, 20
+a, b = b, a
+
+print(a, ", ", b)
