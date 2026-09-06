742ea713b5763bd08bf77fc260e3f8c9e0768eb6
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2026-09-06 19:59:25 +0800

    prac: 7

--- hi.py
+++ hi.py
@@ -1,30 +1,26 @@
-details = [str(""), int(), float(), int()]
+listKo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-details[0] = input("What is your name? ")
+for digit in listKo:
+    divisibles = []
+    if digit % 1 == 0:
+        divisibles.append(1)
+    if digit % 2 == 0:
+        divisibles.append(2)
+    if digit % 3 == 0:
+        divisibles.append(3)
+    if digit % 4 == 0:
+        divisibles.append(4)
+    if digit % 5 == 0:
+        divisibles.append(5)
+    if digit % 6 == 0:
+        divisibles.append(6)
+    if digit % 7 == 0:
+        divisibles.append(7)
+    if digit % 8 == 0:
+        divisibles.append(8)
+    if digit % 9 == 0:
+        divisibles.append(9)
+    if digit % 10 == 0:
+        divisibles.append(10)
 
-validCharactersForName = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ "
-nameHasSpecialDigit = False
-
-for char in details[0]:
-    if char not in validCharactersForName:
-        print(f"Invalid Character: {char}")
-
-details[1] = input("What is your age? ")
-
-validDigits = "0123456789"
-
-for digit in details[1]:
-    if digit not in validDigits:
-        print(f"Invalid Character: {details[1]}")
-
-details[2] = input("What is your height (meters)? ")
-
-details[3] = input("What is your favorite number (whole number only)? ")
-
-for digit in details[3]:
-    if digit not in validDigits:
-        print(f"Invalid Character: {details[3]}")
-
-print(
-    f"Your name is {details[0]}, {details[1]} years old. You are {details[2]} tall. Your favorite number is {details[3]}"
-)
+    print(f"Digit {digit} is divisible be {divisibles}")
