f83a19cb6320cf7cd3da99cca083f1e9403cef44
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2026-09-06 10:06:41 +0800

    prac: 5

--- hi.py
+++ hi.py
@@ -1,6 +1,7 @@
 name = input("What is your name? ")
 
 englishAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+digits = "0123456789"
 
 nameHasSpecialCharac = False
 invalid_char = ""
@@ -10,6 +11,7 @@ for char in name:
     for charEnglishAlphbet in englishAlphabet:
         if char == charEnglishAlphbet:
             char_passed = True
+            break
 
     nameHasSpecialCharac = not char_passed
     invalid_char = char
@@ -19,4 +21,31 @@ for char in name:
 
     print("Character passed: ", char)
 
-print('INVALID: Your name has special character: "', invalid_char, '" ')
+if nameHasSpecialCharac:
+    print(f"INVALID: Your name has special character: {invalid_char}")
+else:
+    print(name[1:3])
+    print(name.upper())
+    print(name.replace("e", "3"))
+    print(name.strip())
+    name = name.strip()
+    names = name.split()
+
+    print(len(names))
+
+    age = input("What is your age? ")
+
+    ageHasInvalidDigit = False
+
+    for digit in age:
+        digit_passed = False
+        for validDigit in digits:
+            if digit == validDigit:
+                digit_passed = True
+
+        if not digit_passed:
+            ageHasInvalidDigit = not digit_passed
+            print(f"INVALID: Your name has invalid digit: {digit}")
+
+    if not ageHasInvalidDigit:
+        print(f"Hello, {name}, you are fucking {age} years old.")
