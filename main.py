eaefb244fad758be4260979b65b2f38ecf2aa620
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2026-09-14 10:27:30 +0800

    prac: classes3

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
-from Classes import Student
-from Enums import Courses
+import random
 
 
-def exercise_1() -> None:
-    person_1: Student.Person = Student.Person("John", 22)
+class BankAccount:
+    @staticmethod
+    def __generate_account_number() -> int:
+        return random.randint(100000000, 999999999)
 
-    student_1: Student.Student = Student.Student("Vemi", 19, Courses.Course.BSBA_MAJ_FM)
-    student_2: Student.Student = Student.Student(
-        "Amoha", 17, Courses.Course.BSBA_MAJ_HRM
-    )
-    student_3: Student.Student = Student.Student("Jole", 60, Courses.Course.BSBA_MAJ_OM)
+    def __init__(self, owner: str, initial_balance: float = 0) -> None:
+        self.account_number: int = self.__generate_account_number()
+        self.owner: str = owner
+        self.balance: float = initial_balance
 
-    person_1.display_information()
-    student_1.display_information()
-    student_2.display_information()
-    student_3.display_information()
+    def deposit(self, amount: float) -> None:
+        if not self.__is_amount_greater_than_zero(amount):
+            return
 
-    print(student_1.is_adult())
+        self.balance += amount
+
+    def withdraw(self, amount: float) -> float:
+        if not self.__is_amount_greater_than_zero(amount):
+            return 0
+
+        if amount > self.balance:
+            print(
+                "ERROR: You are trying to withdraw an amount greater than your balance."
+            )
+            return 0
+
+        print(f"Withdrawing {amount}")
+
+        self.balance -= amount
+
+        return amount
+
+    def get_balance(self) -> float:
+        return self.balance
+
+    def display_account(self) -> None:
+        print(f"""
+            Account Number: {self.account_number}
+            Owner: {self.owner}
+            Balance: {self.balance}
+        """)
+
+    def __is_amount_greater_than_zero(self, amount: float) -> bool:
+        if amount <= 0:
+            print("ERROR: Amount can not be less than zero.")
+            return False
+
+        return True
 
 
-def main() -> None:
-    exercise_1()
+account_1: BankAccount = BankAccount("John", 100)
 
+account_1.display_account()
 
-main()
+account_1.deposit(34829)
+
+account_1.display_account()
+
+account_1.withdraw(9999999)
+
+account_1.withdraw(34929)
+
+account_1.display_account()
