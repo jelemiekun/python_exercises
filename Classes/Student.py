5456125c148ed19017d5474036732f15754c0c72
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2026-09-14 08:23:56 +0800

    prac: classes2

--- Classes/Student.py
+++ Classes/Student.py
@@ -1,18 +1,37 @@
+from typing import override
+
 from Enums import Courses
 
 
-class Student:
-    def __init__(self, name: str, age: int, course: Courses.Course) -> None:
+class Person:
+    def __init__(self, name: str, age: int) -> None:
         self.name: str = name
         self.age: int = age
-        self.course: Courses.Course = course
+
+        print("Person instance created!")
 
     def display_information(self) -> None:
         print("=================================")
         print(f"{self.name}")
         print(f"{self.age}")
-        print(f"{self.course}")
         print("=================================")
 
+    def _do_something(self) -> None:
+        print("Top secret!")
+
+
+class Student(Person):
+    def __init__(self, name: str, age: int, course: Courses.Course) -> None:
+        self.name: str = f"S_{name}"
+        self.age: int = age
+        self.course: Courses.Course = course
+
+        print("Student instance created!")
+
+    @override
+    def display_information(self) -> None:
+        print("POLYMORPHISM!!!!")
+
     def is_adult(self) -> bool:
+        Person._do_something(self)
         return self.age >= 18
--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from Enums import Courses
 
 
 def exercise_1() -> None:
+    person_1: Student.Person = Student.Person("John", 22)
+
     student_1: Student.Student = Student.Student("Vemi", 19, Courses.Course.BSBA_MAJ_FM)
     student_2: Student.Student = Student.Student(
         "Amoha", 17, Courses.Course.BSBA_MAJ_HRM
     )
     student_3: Student.Student = Student.Student("Jole", 60, Courses.Course.BSBA_MAJ_OM)
 
+    person_1.display_information()
     student_1.display_information()
     student_2.display_information()
     student_3.display_information()
+
+    print(student_1.is_adult())
 
 
 def main() -> None:
